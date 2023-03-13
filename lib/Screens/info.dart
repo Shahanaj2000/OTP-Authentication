c9d522e53dd6e2c6eab97558f16f89425f91f9e7
Shahanaj2000 <shahanajmt@gmail.com>
2023-03-13 20:43:29 +0530

    InfoScreen UI completed

--- lib/Screens/info.dart
+++ lib/Screens/info.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:phoneauth_firebase/Widgets/customButton.dart';
 
 class InfoScreen extends StatefulWidget {
   const InfoScreen({super.key});
@@ -9,8 +11,149 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
+  //! Image
+  File? image;
+  //! Controllers
+  final nameController = TextEditingController();
+  final emailConroller = TextEditingController();
+  final bioController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    nameController.dispose();
+    emailConroller.dispose();
+    bioController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 5),
+          child: Center(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: image == null
+                      ? const CircleAvatar(
+                          backgroundColor: Colors.blueAccent,
+                          radius: 50,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        )
+                      : CircleAvatar(
+                          backgroundImage: FileImage(image!),
+                          radius: 50,
+                        ),
+                ),
+                //! TextFeild
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: [
+                      //nameFeild
+                      textFeild(
+                        text: "John Honay",
+                        icon: Icons.account_circle,
+                        inputType: TextInputType.name,
+                        maxLines: 1,
+                        controller: nameController,
+                      ),
+                      //emailFeild
+                      textFeild(
+                        text: "Johnhonay@gmail.com",
+                        icon: Icons.email,
+                        inputType: TextInputType.emailAddress,
+                        maxLines: 1,
+                        controller: emailConroller,
+                      ),
+
+                      //bioFeild
+                      textFeild(
+                        text: "Enter your bio here....",
+                        icon: Icons.edit,
+                        inputType: TextInputType.text,
+                        maxLines: 2,
+                        controller: bioController,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  child: CustomButton(text: "Continue", onPressed: () {},),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //widgetTextFeild
+  Widget textFeild({
+    required String text,
+    required IconData icon,
+    required TextInputType inputType,
+    required int maxLines,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(
+        cursorColor: Colors.blueAccent,
+        controller: controller,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            margin: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blueAccent,
+            ),
+            child: Icon(
+              icon,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+          //!
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          //!
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          hintText: text,
+          alignLabelWithHint: true,
+          border: InputBorder.none,
+          fillColor: Colors.blue.shade50,
+          filled: true,
+        ),
+      ),
+    );
   }
 }
